e370af4b9ac6b217614f2833466ed48e456af7c6
Tom Gray <tomg77@yahoo.co.uk>
2020-06-12 14:20:02 +0100

    Added testing for fraction addition

--- test/SymMat/FractionsSpec.hs
+++ test/SymMat/FractionsSpec.hs
@@ -4,6 +4,15 @@ import SymMat.Fraction
 import Test.QuickCheck
 import Test.Hspec
 
+instance Arbitrary Fraction where
+  arbitrary = do
+    NonZero num <- arbitrary
+    NonZero den <- arbitrary
+    return $ Fraction num den
+
+marginErr :: Float
+marginErr = 0.00001
+
 -- | Tests simplification of fraction doesn't change real value
 prop_simplifyEqual :: Int -> Int -> Bool
 prop_simplifyEqual num 0 = True
@@ -12,8 +21,18 @@ prop_simplifyEqual num den = let
   initVal = toReal $ Fraction num den
   in valSimplified == initVal
 
+-- | Tests addition of fractions
+prop_addition :: Fraction -> Fraction -> Bool
+prop_addition fracA fracB = let
+  eval1 = toReal (fracA + fracB)
+  eval2 = (toReal fracA) + (toReal fracB)
+  in abs(eval1 - eval2) < marginErr -- Can't test for equality due to floating point errors
+
 spec :: Spec
 spec = do
   describe "Fractions" $ do
     it "Simplified fraction is still equal to unsimplified fraction" $
       property prop_simplifyEqual
+
+    it "Adding fractions works as expected" $
+      property prop_addition
