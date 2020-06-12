941ea9603b8889a868b309cb3a5bca35ad8bbc4d
Tom Gray <tomg77@yahoo.co.uk>
2020-06-12 13:58:13 +0100

    Implemented math operations for fraction type

--- src/SymMat/Fraction.hs
+++ src/SymMat/Fraction.hs
@@ -12,9 +12,18 @@ data Fraction = Fraction {
   denominator :: Int
 } deriving (Eq,Show)
 
+instance Num (Fraction) where
+  Fraction a b + Fraction c d = Fraction (a*d + b*c) (b*d)
+  Fraction a b * Fraction c d = Fraction (a*c) (b*d)
+  Fraction a b - Fraction c d = Fraction (a*d - b*c) (b*d)
+  abs (Fraction a b) = Fraction (abs a) (abs b)
+  signum (Fraction a b) = Fraction (signum a) (signum b)
+  fromInteger i = Fraction (fromInteger i) 1
+
 toReal :: Fraction -> Float
 toReal (Fraction num den) = (fromIntegral num) / (fromIntegral den)
 
+-- | Simplifies a fraction
 simplify :: Fraction -> Fraction
 simplify (Fraction num 0) = Fraction num 0
 simplify (Fraction num den) = let
